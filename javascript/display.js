d2b402a18ac69b94dc8b7af5ef22c22f67506156
yannick biot <yannickpro64@gmail.com>
2025-05-11 17:19:43 +0200

    ajout d'un formattage de array lorsqu'on check une tache, avec un nouveau display

--- javascript/display.js
+++ javascript/display.js
@@ -1,19 +1,21 @@
 import { updateLocalStorage } from "./storage.js";
 
 // Initialise des sons pour qu'il ne soit chargé qu'une fois
-const erase_sound = new Audio("./sound/erase.wav")
+const erase_sound = new Audio("./sound/erase.wav");
 erase_sound.volume = 0.4;
-const add_sound = new Audio("./sound/pen.wav")
+const add_sound = new Audio("./sound/pen.wav");
 add_sound.volume = 0.05;
 
 // Variable pour savoir si le panet est en mode validation afin de configurer
 // l'action de Enter dans l'input
-export let isPanelValidationMode = true
+export let isPanelValidationMode = true;
 
 // Générer l'affichage des todos dans #liste
 // array: le tableau a afficher
 // action: permet de personnaliser un peu la fonction par rapport au contexte
 export function displayList(array, action) {
+  // formattage de array pour que les actions checkées soient en dernière position
+  array = formattage(array);
   const ul = document.getElementById("liste");
   array.forEach((todo, index, array) => {
     // Creation des li
@@ -23,45 +25,43 @@ export function displayList(array, action) {
   // réinitialise input et focus
   document.getElementById("input").value = "";
   document.getElementById("input").focus();
-  action === "add" && add_sound.play()
-  action === "supp" && erase_sound.play()
+  action === "add" && add_sound.play();
+  action === "supp" && erase_sound.play();
 }
 
 export function initListe() {
   document.getElementById("liste").innerHTML = "";
 }
 
-export function returnToPanelValidation(){
-    // je cache le bouton update et je fais apparaitre le bouton add
-    document.getElementById("add").classList.remove("btn_round-hiddened")
-    document.getElementById("update").classList.add("btn_round-hiddened")
-    // je change le style du focus de input
-    document.getElementById("input").classList.remove("modifMod")
-    // je cache le bouton annuler et je fais apparaitre le bouton supprimer
-    document.getElementById("supp").classList.remove("btn-hiddened")
-    document.getElementById("cancel").classList.add("btn-hiddened")
-    // Je réinitialise le champ de saisie
-    document.getElementById("input").value = ""
-    // J'annule les modificateurs de modif sur les li
-    init_modif_value()
-    // je focus sur le champ de saisie
-    document.getElementById("input").focus()
-    // je change l'état de la variable isPanelValidationMode
-    isPanelValidationMode = true
+export function returnToPanelValidation() {
+  // je cache le bouton update et je fais apparaitre le bouton add
+  document.getElementById("add").classList.remove("btn_round-hiddened");
+  document.getElementById("update").classList.add("btn_round-hiddened");
+  // je change le style du focus de input
+  document.getElementById("input").classList.remove("modifMod");
+  // je cache le bouton annuler et je fais apparaitre le bouton supprimer
+  document.getElementById("supp").classList.remove("btn-hiddened");
+  document.getElementById("cancel").classList.add("btn-hiddened");
+  // Je réinitialise le champ de saisie
+  document.getElementById("input").value = "";
+  // J'annule les modificateurs de modif sur les li
+  init_modif_value();
+  // je focus sur le champ de saisie
+  document.getElementById("input").focus();
+  // je change l'état de la variable isPanelValidationMode
+  isPanelValidationMode = true;
 }
 
-export function erase_last_one(array){
-    array.pop()
-    updateLocalStorage(array)
-    initListe()
-    displayList(array, "supp")
+export function erase_last_one(array) {
+  array.pop();
+  updateLocalStorage(array);
+  initListe();
+  displayList(array, "supp");
 }
 
 //
 // Fonctions private
 //
-
-
 
 function create_li(todo, index, array, action) {
   const li = document.createElement("li");
@@ -77,15 +77,15 @@ function create_li(todo, index, array, action) {
     // je met en valeur l'élément en train d'être modifié
     li.classList.add("modif_up");
     // je cache le bouton add et je fais apparaitre le bouton update
-    document.getElementById("add").classList.add("btn_round-hiddened")
-    document.getElementById("update").classList.remove("btn_round-hiddened")
+    document.getElementById("add").classList.add("btn_round-hiddened");
+    document.getElementById("update").classList.remove("btn_round-hiddened");
     // je change le style du focus de input
-    document.getElementById("input").classList.add("modifMod")
+    document.getElementById("input").classList.add("modifMod");
     // je cache le bouton supprimer et je fais apparaitre le bouton annuler
-    document.getElementById("supp").classList.add("btn-hiddened")
-    document.getElementById("cancel").classList.remove("btn-hiddened")
+    document.getElementById("supp").classList.add("btn-hiddened");
+    document.getElementById("cancel").classList.remove("btn-hiddened");
     // je modifie l'indicateur de isPanelValidatinMode
-    isPanelValidationMode = false
+    isPanelValidationMode = false;
   });
 
   const p = document.createElement("p");
@@ -104,18 +104,27 @@ function create_li(todo, index, array, action) {
     array[index].isChecked = checkbox.checked;
     updateLocalStorage(array);
     li.classList.toggle("to_supp");
+    initListe();
+    displayList(array, "supp");
   });
   li.appendChild(p);
   li.appendChild(checkbox);
   // Je marque le p du dernier li pour faire une animamation dessus si c'est une action add
-  if((index == array.length - 1) && action === "add") li.querySelector("p").classList.add("last_one")
+  if (index == array.length - 1 && action === "add")
+    li.querySelector("p").classList.add("last_one");
   return li;
 }
 
 // Fonction pour annuler les modificateurs de modif sur les li
 function init_modif_value() {
-    const all_li = document.querySelectorAll("#container li");
-    all_li.forEach((li) => {
-      li.classList.remove("modif_up");
-    });
-  }
+  const all_li = document.querySelectorAll("#container li");
+  all_li.forEach((li) => {
+    li.classList.remove("modif_up");
+  });
+}
+
+function formattage(array) {
+  const array_unchecked = array.filter((element) => !element.isChecked);
+  const array_checked = array.filter((element) => element.isChecked);
+  return array_unchecked.concat(array_checked);
+}
